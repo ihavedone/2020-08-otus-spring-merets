ac4ff06bf235cb9004f0e20ace8cbc2222db8dfb
Artem Merets <artem@merets.ru>
2020-09-20 16:34:18 +0300

    init a new project

---
+++ library/src/test/java/ru/otus/merets/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.otus.merets.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
