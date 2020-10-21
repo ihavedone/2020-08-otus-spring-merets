cbda93f9ed587e64132ff620d6f6dd7f06448f63
Artem Merets <artem@merets.ru>
2020-10-21 20:58:13 +0300

    en: some little fixes ru: небольшие правки

--- library/src/main/java/ru/otus/merets/library/domain/Book.java
+++ library/src/main/java/ru/otus/merets/library/domain/Book.java
@@ -3,7 +3,8 @@ package ru.otus.merets.library.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.util.Objects;
@@ -23,12 +24,14 @@ public class Book {
     @Column(name = "caption")
     private String caption;
 
-    @BatchSize(size = 10)
+//    @BatchSize(size = 10)
+    @Fetch(FetchMode.SUBSELECT)
     @ManyToMany(targetEntity = Author.class, fetch = FetchType.EAGER)
     @JoinTable(name= "books_authors", joinColumns = @JoinColumn(name="book_id"),inverseJoinColumns = @JoinColumn(name="author_id"))
     private Set<Author> authors;
 
-    @BatchSize(size = 10)
+//    @BatchSize(size = 10)
+    @Fetch(FetchMode.SUBSELECT)
     @ManyToMany(targetEntity = Genre.class, fetch = FetchType.EAGER)
     @JoinTable(name= "books_genres", joinColumns = @JoinColumn(name="book_id"),inverseJoinColumns = @JoinColumn(name="genre_id"))
     private Set<Genre> genres;
--- library/src/main/java/ru/otus/merets/library/domain/Comment.java
+++ library/src/main/java/ru/otus/merets/library/domain/Comment.java
@@ -20,7 +20,7 @@ public class Comment {
     @Column(name = "text")
     private String text;
 
-    @ManyToOne(targetEntity = Book.class, fetch = FetchType.EAGER)
+    @ManyToOne(targetEntity = Book.class, fetch = FetchType.LAZY)
     @JoinColumn(name = "book_id")
     private Book book;
 }
